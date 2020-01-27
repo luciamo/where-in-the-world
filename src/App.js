0d763e792592da9206d09887bc0ab49bac22f963
LucianoDantas <luciano.dantas@codeminer42.com>
2020-01-26 22:23:22 -0300

    Set the enviroment

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.scss';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Home from './pages/Home';
+import Country from './pages/Country';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/country/:alpha3Code' component={Country} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
